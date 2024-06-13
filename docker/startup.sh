a311b74bf7cc6199addab2ef25f4963690d62544
MrH <h@yden.au>
2024-06-13 14:36:15 +1000

    Update startup.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 WORKDIR=`pwd`
 CFGFILE="$WORKDIR/config.yml"
@@ -8,5 +8,30 @@ if [ ! -f "$CFGFILE" ]; then
     exit 1
 fi
 
+PORT=587  # Default Port
+
+found_receive=0
+while IFS= read -r line; do
+    # Remove all whitespace from the line
+    trimmed_line=$(echo "$line" | tr -d '[:space:]')
+
+    # Check if the line starts with "receive:"
+    if [[ $trimmed_line == receive:* ]]; then
+        found_receive=1
+        continue
+    fi
+
+    # If "receive:" line was found, look for the next "port:" line
+    if [[ $found_receive -eq 1 && $trimmed_line == *port:* ]]; then
+        # Check if the line starts with #
+        if ! [[ $trimmed_line == \#* ]]; then
+            # Split the trimmed line by ":" and get the port number
+            PORT=$(echo "$trimmed_line" | cut -d':' -f2)
+        fi
+        break
+    fi
+done < "$CFGFILE"
+
+
 # Run SMTP2Graph
-node /bin/smtp2graph.js --receive.port=587
+node /bin/smtp2graph.js --receive.port=$PORT
